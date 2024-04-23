404f3423f462b1da9331794a3602d50fe8462b45
Richard Chukwu <richinex@gmail.com>
2024-04-23 21:34:09 +0200

    added cicd.yaml

--- src/components/gallery.rs
+++ src/components/gallery.rs
@@ -11,22 +11,22 @@ pub fn gallery() -> Html {
                 <p class="text-center mb-4">{ "Some of my favorite projects." }</p>
                 <div class="gallery-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     <GalleryItem
-                        image_src="images/organized_folder_tree.png"
+                        image_src="/richard-chukwu/images/organized_folder_tree.png"
                         title="Plexisort"
                         description="An OS agnostic folder organizer."
                     />
                     <GalleryItem
-                        image_src="images/project2.png"
+                        image_src="/richard-chukwu/images/project2.png"
                         title="Project X"
                         description="A revolutionary new app that changes the way you do business."
                     />
                     <GalleryItem
-                        image_src="images/project3.png"
+                        image_src="/richard-chukwu/images/project3.png"
                         title="Project Y"
                         description="A groundbreaking new platform for professional networking."
                     />
                     <GalleryItem
-                        image_src="images/organized_folder_tree.png"
+                        image_src="/richard-chukwu/images/organized_folder_tree.png"
                         title="Project A"
                         description="A comprehensive tool for data analysis."
                     />

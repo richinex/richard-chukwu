c463582e1403d1ee590c683f16695e3018e82757
Richard Chukwu <richinex@gmail.com>
2024-04-23 22:07:21 +0200

    added cicd.yaml

--- src/app.rs
+++ src/app.rs
@@ -1,53 +1,3 @@
-// use yew::functional::*;
-// use yew::prelude::*;
-// use crate::components::{footer, gallery, home, projects, secure, navbar, articles};
-// use yew_router::prelude::*;
-
-// #[derive(Debug, Clone, Copy, PartialEq, Routable)]
-// pub enum Route {
-//     #[at("/")]
-//     Home,
-//     #[at("/projects")]
-//     Projects,
-//     #[at("/gallery")]
-//     Gallery,
-//     #[at("/secure")]
-//     Secure,
-//     #[at("/article")]
-//     Articles,
-//     #[not_found]
-//     #[at("/404")]
-//     NotFound,
-// }
-
-// #[function_component(App)]
-// pub fn app() -> Html {
-//     html! {
-//         <BrowserRouter>
-//             <div class="flex flex-col min-h-screen">
-//                 <navbar::NavBar />
-//                 <main class="flex-grow">
-//                 <Switch<Route> render={switch} />
-//                 </main>
-//                 <footer::Footer />
-//             </div>
-//         </BrowserRouter>
-//     }
-// }
-
-// pub fn switch(routes: Route) -> Html {
-//     web_sys::console::log_1(&format!("Routing to {:?}", routes).into());
-//     match routes {
-//         Route::Home => html! { <home::Home /> },
-//         Route::Projects => html! { <projects::Projects /> },
-//         Route::Gallery => html! { <gallery::Gallery /> },
-//         Route::Secure => html! { <secure::Secure /> },
-//         Route::Articles => html! { <articles::Articles /> },
-//         Route::NotFound => html! { <h1>{"404 Not Found"}</h1> },
-//     }
-// }
-
-
 use yew::functional::*;
 use yew::prelude::*;
 use crate::components::{footer, gallery, home, projects, secure, navbar, articles};
@@ -63,7 +13,7 @@ pub enum Route {
     Gallery,
     #[at("/secure")]
     Secure,
-    #[at("/articles")]
+    #[at("/article")]
     Articles,
     #[not_found]
     #[at("/404")]
@@ -72,22 +22,6 @@ pub enum Route {
 
 #[function_component(App)]
 pub fn app() -> Html {
-    let navigator = use_navigator().expect("Navigator should be available");
-
-    let switch = move |routes: Route| -> Html {
-        match routes {
-            Route::Home => html! { <home::Home /> },
-            Route::Projects => html! { <projects::Projects /> },
-            Route::Gallery => html! { <gallery::Gallery /> },
-            Route::Secure => html! { <secure::Secure /> },
-            Route::Articles => html! { <articles::Articles /> },
-            Route::NotFound => {
-                navigator.push(&Route::Home);
-                html! { <home::Home /> } // Optionally show a redirecting message or spinner here
-            },
-        }
-    };
-
     html! {
         <BrowserRouter>
             <div class="flex flex-col min-h-screen">
@@ -100,3 +34,69 @@ pub fn app() -> Html {
         </BrowserRouter>
     }
 }
+
+pub fn switch(routes: Route) -> Html {
+    web_sys::console::log_1(&format!("Routing to {:?}", routes).into());
+    match routes {
+        Route::Home => html! { <home::Home /> },
+        Route::Projects => html! { <projects::Projects /> },
+        Route::Gallery => html! { <gallery::Gallery /> },
+        Route::Secure => html! { <secure::Secure /> },
+        Route::Articles => html! { <articles::Articles /> },
+        Route::NotFound => html! { <home::Home /> },
+    }
+}
+
+
+// use yew::functional::*;
+// use yew::prelude::*;
+// use crate::components::{footer, gallery, home, projects, secure, navbar, articles};
+// use yew_router::prelude::*;
+
+// #[derive(Debug, Clone, Copy, PartialEq, Routable)]
+// pub enum Route {
+//     #[at("/")]
+//     Home,
+//     #[at("/projects")]
+//     Projects,
+//     #[at("/gallery")]
+//     Gallery,
+//     #[at("/secure")]
+//     Secure,
+//     #[at("/articles")]
+//     Articles,
+//     #[not_found]
+//     #[at("/404")]
+//     NotFound,
+// }
+
+// #[function_component(App)]
+// pub fn app() -> Html {
+//     let navigator = use_navigator().expect("Navigator should be available");
+
+//     let switch = move |routes: Route| -> Html {
+//         match routes {
+//             Route::Home => html! { <home::Home /> },
+//             Route::Projects => html! { <projects::Projects /> },
+//             Route::Gallery => html! { <gallery::Gallery /> },
+//             Route::Secure => html! { <secure::Secure /> },
+//             Route::Articles => html! { <articles::Articles /> },
+//             Route::NotFound => {
+//                 navigator.push(&Route::Home);
+//                 html! { <home::Home /> } // Optionally show a redirecting message or spinner here
+//             },
+//         }
+//     };
+
+//     html! {
+//         <BrowserRouter>
+//             <div class="flex flex-col min-h-screen">
+//                 <navbar::NavBar />
+//                 <main class="flex-grow">
+//                 <Switch<Route> render={switch} />
+//                 </main>
+//                 <footer::Footer />
+//             </div>
+//         </BrowserRouter>
+//     }
+// }

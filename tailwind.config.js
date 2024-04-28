d3cc87bf56755b12a7ab3911b2e731278354a234
Richard Chukwu <richinex@gmail.com>
2024-04-28 22:27:04 +0200

    Added images to gallery

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,71 +1,73 @@
 
 // tailwind.config.js
-module.exports = {
+// module.exports = {
+//     content: [
+//         "./src/**/*.rs", // Path to your Rust component files
+//         "./static/**/*.html" // Any other HTML files
+//       ],
+//     darkMode: 'class', // or 'media'
+//     theme: {
+//       extend: {
+//         colors: {
+//             'custom-black': '#0a0a0a', // Define a custom black if needed
+//             'bright-blue': '#0066ff',  // Define a custom blue if needed
+//             'rich-blue': '#243c5a',
+//             'rich-accent': '#FF6B6B',
+//             'primary': '#f5f5f5',
+//             'background': '#121212',
+//             // Define other colors as needed
+//           },
+//       },
+//     },
+//     // Ensure other needed configurations are set
+//   };
+
+  module.exports = {
     content: [
-        "./src/**/*.rs", // Path to your Rust component files
-        "./static/**/*.html" // Any other HTML files
-      ],
-    darkMode: 'class', // or 'media'
+      "./src/**/*.rs", // Path to your Rust component files
+      "./static/**/*.html" // Any other HTML files
+    ],
+    darkMode: ["class", '[data-mode="theme-dark"]'],
     theme: {
       extend: {
-        colors: {
-            'rich-blue': '#243c5a',
-            'rich-accent': '#FF6B6B',
-            'primary': '#f5f5f5',
-            'background': '#121212',
-            // Define other colors as needed
-          },
+        transitionDuration: {
+          0: "0ms",
+          400: "400ms",
+          450: "450ms",
+        },
+        width: {
+          128: "32rem",
+        },
+      },
+      themes: {
+        dark: {
+          white: "#fff",
+          black: "#000",
+          "text-main": "#fff",
+          "accent-primary": "#2563EB",
+          "accent-tinted": "#3897e9",
+          "accent-shaded": "#1f7dcf",
+          "bg-main": "#111",
+          "bg-secondary": "#191919",
+          "muted-main": "#E6E6E6",
+          "muted-secondary": "#CCCCCC",
+        },
+      },
+      colors: {
+        white: "#000",
+        black: "#fff",
+        "accent-primary": "#228be6",
+        "accent-tinted": "#3897e9",
+        "accent-shaded": "#1f7dcf",
+        "text-main": "#111",
+        "bg-main": "#fff",
+        "bg-secondary": "#333",
+        "muted-main": "#fff",
+        "muted-secondary": "#fff",
       },
     },
-    // Ensure other needed configurations are set
+    transitionProperty: {
+      scale: "scale",
+    },
+
   };
-
-  // module.exports = {
-  //   content: [
-  //     "./src/**/*.rs", // Path to your Rust component files
-  //     "./static/**/*.html" // Any other HTML files
-  //   ],
-  //   darkMode: ["class", '[data-mode="theme-dark"]'],
-  //   theme: {
-  //     extend: {
-  //       transitionDuration: {
-  //         0: "0ms",
-  //         400: "400ms",
-  //         450: "450ms",
-  //       },
-  //       width: {
-  //         128: "32rem",
-  //       },
-  //     },
-  //     themes: {
-  //       dark: {
-  //         white: "#fff",
-  //         black: "#000",
-  //         "text-main": "#fff",
-  //         "accent-primary": "#2563EB",
-  //         "accent-tinted": "#3897e9",
-  //         "accent-shaded": "#1f7dcf",
-  //         "bg-main": "#111",
-  //         "bg-secondary": "#191919",
-  //         "muted-main": "#E6E6E6",
-  //         "muted-secondary": "#CCCCCC",
-  //       },
-  //     },
-  //     colors: {
-  //       white: "#000",
-  //       black: "#fff",
-  //       "accent-primary": "#228be6",
-  //       "accent-tinted": "#3897e9",
-  //       "accent-shaded": "#1f7dcf",
-  //       "text-main": "#111",
-  //       "bg-main": "#fff",
-  //       "bg-secondary": "#333",
-  //       "muted-main": "#fff",
-  //       "muted-secondary": "#fff",
-  //     },
-  //   },
-  //   transitionProperty: {
-  //     scale: "scale",
-  //   },
-
-  // };
